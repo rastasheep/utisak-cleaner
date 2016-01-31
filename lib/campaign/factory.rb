bc940d52fe66d60f352f25aec2f2b778b2bd29a9
rastasheep <rastasheep3@gmail.com>
2016-01-31 19:05:59 +0100

    Send both daily and weekly newsletters on saturdays

--- lib/campaign/factory.rb
+++ lib/campaign/factory.rb
@@ -7,18 +7,14 @@ class Campaign::Factory
 
   attr_reader :campaign
 
-  def initialize
-    @campaign = campaign_class.new(:debug => debug, :segment_id => segment_id)
-  end
-
   def execute
-    @campaign.execute
+    campaigns.map(&:execute)
   end
 
   private
 
-  def version
-    ENV["CAMPAIGN"] || campaign_version_from_date
+  def versions
+    ENV["CAMPAIGN"] || campaign_version_for_date
   end
 
   def debug
@@ -29,11 +25,15 @@ class Campaign::Factory
     ENV["SEGMENT"]
   end
 
-  def campaign_class
+  def campaigns
+    versions.to_a.map{|version| campaign_class(version).new(:debug => debug, :segment_id => segment_id)}
+  end
+
+  def campaign_class(version)
     Object.const_get("Campaign::#{version.capitalize}")
   end
 
-  def campaign_version_from_date
-    Time.now.saturday? ? WEEKLY : DAILY
+  def campaign_version_for_date
+    Time.now.saturday? ? [DAILY, WEEKLY] : [DAILY]
   end
 end
